9dbd1ef7ad10a43ce986f5a7bd44cebeeb40d9e7
mirzayogy <mirza.yogy@gmail.com>
2024-04-24 20:35:06 +0800

    Initial commit

---
+++ InputOutput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class InputOutput{
+    public static void main(String[] args){
+        int nilai = 0;
+        String nama;
+        //Class object = new Constructor();
+        Scanner scanner = new Scanner(System.in);
+
+        nama = scanner.nextLine();
+        System.out.println(nama);
+    }
+}
